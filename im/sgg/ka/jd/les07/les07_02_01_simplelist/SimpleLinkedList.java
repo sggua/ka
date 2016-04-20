7d7125733889f63823524b3c0f7f2b5bc3c5399d
Sergiy Gorichenko <m@sgg.im>
2016-04-20 19:59:24 +0300

    les 04 / 02 / 01 own list

---
+++ im/sgg/ka/jd/les07/les07_02_01_simplelist/SimpleLinkedList.java
@@ -0,0 +1,27 @@
+package im.sgg.ka.jd.les07.les07_02_01_simplelist;
+
+public class SimpleLinkedList {
+    private Node root;
+    private int size;
+
+
+    public SimpleLinkedList() {
+        this(null,0);
+    }
+
+    public SimpleLinkedList(Node root, int size) {
+        this.root = root;
+        this.size = size;
+    }
+
+    public void addFirst(Object o) {
+    }
+    public void addLast(Object o) {
+    }
+    public void addAfter(Object o1,Object o2) {
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
